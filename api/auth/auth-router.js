94415abd5cefe64288699c92a1452697461a55f2
Jack Nunnink <jack.nunnink@protonmail.com>
2022-06-21 17:54:13 -0400

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,8 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
 const router = require("express").Router();
+const User = require('../users/users-model')
+const bcrypt = require('bcryptjs')
 const {
   checkUsernameFree,
   checkUsernameExists,
@@ -30,7 +32,14 @@ const {
   }
  */
 router.post("/register", checkPasswordLength, checkUsernameFree, (req, res, next) => {
-  res.json('register');
+  const { username, password } = req.body
+  const hash = bcrypt.hashSync(password, 12)
+
+  User.add({ username, password: hash })
+    .then(saved => {
+      res.status(201).json(saved)
+    })
+    .catch(next)
 })
 /**
  2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -48,7 +57,13 @@ router.post("/register", checkPasswordLength, checkUsernameFree, (req, res, next
   }
   */
 router.post("/login", checkUsernameExists, (req, res, next) => {
-  res.json('login');
+  const { password } = req.body
+  if (bcrypt.compareSync(password, req.user.password)) {
+    req.session.user = req.user
+    res.json({ message: `Welcome ${req.user.username}` })
+  } else {
+    next({ status: 401, message: "Invalid credentials" })
+  }
 })
 /**
   3 [GET] /api/auth/logout
